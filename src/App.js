20136b3d2a43422b678eccaa362bc1811905d88b
NateS2 <nathan.stec9@gmail.com>
2018-07-02 14:27:48 -0400

    Incorporate live data

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const TopImage = () => (
     // fluid
     src="https://source.unsplash.com/random"
     style={{
-      marginTop: -150,
+      marginTop: -120, //-150
       marginLeft: -50,
       backgroundColor: "#fff",
       padding: 4
@@ -72,26 +72,10 @@ const RightImage = () => (
   />
 );
 
-const Paragraph = () => (
-  <p>
-    {[
-      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ",
-      "tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas ",
-      "semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ",
-      "ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean ",
-      "fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. ",
-      "Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor ",
-      "neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, ",
-      "accumsan porttitor, facilisis luctus, metus"
-    ].join("")}
-  </p>
-);
-
 export default class StickyLayout extends Component {
   state = {
-    menuFixed: false,
-    overlayFixed: false,
-    post: { body: "", images: [], title: "", creator: {} }
+    post: { body: "", images: [], title: "", creator: "" },
+    paragraphs: []
   };
 
   async componentDidMount() {
@@ -99,6 +83,7 @@ export default class StickyLayout extends Component {
     const post = await zupage.getCurrentPost();
     this.setState({ post });
     console.log("Response!", post);
+    this.createParagraphs();
   }
 
   imageTagCreater = images => {
@@ -107,28 +92,24 @@ export default class StickyLayout extends Component {
     });
   };
 
-  handleOverlayRef = c => {
-    const { overlayRect } = this.state;
-
-    if (!overlayRect) {
-      this.setState({
-        overlayRect: _.pick(c.getBoundingClientRect(), "height", "width")
-      });
-    }
+  createParagraphs = () => {
+    const { body } = this.state.post;
+    var paragraphBody = body.split(/[\r\n\t]+/gm);
+    this.setState({ paragraphs: paragraphBody });
   };
 
-  stickOverlay = () => this.setState({ overlayFixed: true });
-
-  stickTopMenu = () => this.setState({ menuFixed: true });
-
-  unStickOverlay = () => this.setState({ overlayFixed: false });
-
-  unStickTopMenu = () => this.setState({ menuFixed: false });
+  formatBody = () => {
+    const { paragraphs } = this.state;
+    const { images } = this.state.post;
+    const paragraphCount = paragraphs.length;
+    const imagecount = images.length;
+    // image[0] will be used for heroHeader
+  };
 
   render() {
-    const { menuFixed, overlayFixed, overlayRect } = this.state;
     const { images, body, title, creator } = this.state.post;
-
+    const { paragraphs } = this.state;
+    console.log("render paragraph", paragraphs);
     return (
       <div>
         {/* Heads up, style below isn't necessary for correct work of example, simply our docs defines other
@@ -139,134 +120,41 @@ export default class StickyLayout extends Component {
             background: #fff;
           }
         `}</style>
-        <HeroHeader title={title} creator={creator} />
+        <HeroHeader title={title} creator={creator} images={images} />
         <Container text style={{ marginTop: "2em" }}>
           <TopImage />
           <Header as="h1">Sticky Header</Header>
         </Container>
-
-        {/* Attaching the top menu is a simple operation, we only switch `fixed` prop and add another style if it has
-            gone beyond the scope of visibility
-          */}
-
-        <Container text>
-          {_.times(3, i => <Paragraph key={i} />)}
-
-          {/* Example with overlay menu is more complex, SUI simply clones all elements inside, but we should use a
-              different approach.
-              An empty Visibility element controls the need to change the fixing of element below, it also uses height
-              and width params received from its ref for correct display.
-            */}
-          <Visibility
-            offset={80}
-            once={false}
-            onTopPassed={this.stickOverlay}
-            onTopVisible={this.unStickOverlay}
-            style={overlayFixed ? { ...overlayStyle, ...overlayRect } : {}}
-          />
-
-          <div
-            ref={this.handleOverlayRef}
-            style={overlayFixed ? fixedOverlayStyle : overlayStyle}>
-            <Menu
-              icon="labeled"
-              style={overlayFixed ? fixedOverlayMenuStyle : overlayMenuStyle}
-              vertical>
-              <Menu.Item>
-                <Icon name="twitter" />
-                Twitter
-              </Menu.Item>
-
-              <Menu.Item>
-                <Icon name="facebook" />
-                Share
-              </Menu.Item>
-
-              <Menu.Item>
-                <Icon name="mail" />
-                Email
-              </Menu.Item>
-            </Menu>
-          </div>
-
-          {_.times(3, i => <Paragraph key={i} />)}
-          <LeftImage />
-
-          <Paragraph />
-          <RightImage />
-
-          {_.times(4, i => <Paragraph key={i} />)}
-          <LeftImage />
-
-          <Paragraph />
-          <RightImage />
-
-          {_.times(2, i => <Paragraph key={i} />)}
-        </Container>
-
-        <Segment
-          inverted
-          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-          vertical>
-          <Container textAlign="center">
-            <Grid columns={4} divided stackable inverted>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header inverted as="h4" content="Group 1" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as="h4" content="Group 2" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as="h4" content="Group 3" />
-                  <List link inverted>
-                    <List.Item as="a">Link One</List.Item>
-                    <List.Item as="a">Link Two</List.Item>
-                    <List.Item as="a">Link Three</List.Item>
-                    <List.Item as="a">Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as="h4" content="Footer Header" />
-                  <p>
-                    Extra space for a call to action inside the footer that
-                    could help re-engage users.
-                  </p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Divider inverted section />
-            <List horizontal inverted divided link>
-              <List.Item as="a" href="#">
-                Site Map
-              </List.Item>
-              <List.Item as="a" href="#">
-                Contact Us
-              </List.Item>
-              <List.Item as="a" href="#">
-                Terms and Conditions
-              </List.Item>
-              <List.Item as="a" href="#">
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
+        <Container text>{paragraphs[0]}</Container>
       </div>
     );
   }
+}
+
+{
+  /* <div
+  ref={this.handleOverlayRef}
+  style={overlayFixed ? fixedOverlayStyle : overlayStyle}>
+  <Menu
+    icon="labeled"
+    style={overlayFixed ? fixedOverlayMenuStyle : overlayMenuStyle}
+    vertical>
+    <Menu.Item>
+      <Icon name="twitter" />
+      Twitter
+    </Menu.Item>
+
+    <Menu.Item>
+      <Icon name="facebook" />
+      Share
+    </Menu.Item>
+
+    <Menu.Item>
+      <Icon name="mail" />
+      Email
+    </Menu.Item>
+  </Menu>
+</div> */
 }
 
 // import React, { Component } from "react";
--- src/heroHeader.js
+++ src/heroHeader.js
@@ -15,6 +15,7 @@ class HeroHeader extends Component {
       creator: newProps.creator
     });
     console.log("did await", this.state);
+    this.setHeroImage();
   }
 
   changeTextSize = title => {
@@ -29,11 +30,22 @@ class HeroHeader extends Component {
     }
   };
 
+  setHeroImage = () => {
+    if (typeof this.state.images == "undefined") {
+      this.setState({
+        imageSRC:
+          "https://images.unsplash.com/photo-1529446253549-fdcd7d925106?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd224123c1c190693663a5a035ed2a38&auto=format&fit=crop&w=1950&q=80"
+      });
+    } else if (this.state.images.length > 0) {
+      this.setState({ imageSRC: this.state.images[0].url });
+    }
+  };
+
   render() {
     return (
       <div>
         <LazyHero
-          imageSrc="https://images.unsplash.com/photo-1529446253549-fdcd7d925106?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd224123c1c190693663a5a035ed2a38&auto=format&fit=crop&w=1950&q=80" //imageSrc={this.state.imageSRC}//
+          imageSrc={this.state.imageSRC} //
           parallaxOffset={100}
           color="#000000"
           opacity={0.1}
@@ -43,7 +55,11 @@ class HeroHeader extends Component {
           {this.changeTextSize(this.state.title)}
           <Label
             attached="bottom right"
-            style={{ marginBottom: 5, marginRight: 5 }}>
+            style={{
+              marginBottom: 5,
+              marginRight: 5,
+              zIndex: 10
+            }}>
             <Image avatar src={this.state.creator.profile_image_url} />
             {this.state.creator.name}
           </Label>
